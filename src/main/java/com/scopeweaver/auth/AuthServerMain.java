a4a225ffc381e5cd1c315889f4da34be471d0e73
Vineet Tyagi <vineettyagi28@gmail.com>
2017-09-18 21:41:36 +0530

    Initial Commit

---
+++ src/main/java/com/scopeweaver/auth/AuthServerMain.java
@@ -0,0 +1,13 @@
+package com.scopeweaver.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthServerMain {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthServerMain.class, args);
+	}
+
+}
